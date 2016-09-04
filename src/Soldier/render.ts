07e1ece26d34908d2e12f46186fd126cf58da39d
mhcurylo <mhcurylo@gmail.com>
2016-09-04 11:01:44 +0100

    Active player display

--- src/Soldier/render.ts
+++ src/Soldier/render.ts
@@ -15,6 +15,7 @@ export function squadRender(soldiers: Soldier[], state: GameState, full?: boolea
 			      ${soldier.classNames ? soldier.classNames : ''}">
      <div class="s-code">${soldier.code}</div>
      <div class="s-name">${soldier.name}</div>
+     ${(full && act) ? '<div class="s-player"> P' + (soldier.player + 1) + '</div>' : ''}
      ${(full && act && !soldier.KIA) ? acts() : ''}
     </div>`; 
 
